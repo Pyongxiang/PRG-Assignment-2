dd1862ed232aa3d7a266c5ec8e61483df52c63c1
Pyongxiang <s10258483@connect.np.edu.sg>
2024-01-15 01:27:27 +0800

    Customer class (Imcomplete)

---
+++ PRG-Assignment-2/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Assignment_2
+{
+    class Customer
+    {
+        //Properties
+        public string Name { get; set; }
+
+        public int MemberId { get; set; }
+
+        public DateTime Dob { get; set; }
+
+        public Order CurrentOrder { get; set; }
+
+        public List<Order> OrderHistory { get; set; }
+
+        public PointCard Rewards { get; set; }
+
+        //Constructors
+        public Customer() { }
+
+        public Customer(string name, int memberId, DateTime dob)
+        {
+            Name = name;
+            MemberId = memberId;
+            Dob = dob;
+        }
+
+        //Method
+        public Order MakeOrder()
+        {
+
+        }
+
+
+
+    }
+}
--- PRG-Assignment-2/Program.cs
+++ PRG-Assignment-2/Program.cs
@@ -2,8 +2,8 @@
 // Student Number : S10258483F
 // Student Name : Yong Xiang
 // Student Number : S10257401E
-// Student Name : Xavier
-// Person who committed : Xavier Lian
+// Student Name : Xavier 
+// Person who committed : Yong Xiang
 //===============================//
 
 
