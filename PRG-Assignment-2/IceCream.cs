7b778e822af02b09432375469c7f671aa879ed30
Xavier Lian <S10257401@connect.np.edu.sg>
2024-01-15 00:54:50 +0800

    IceCream Abstract Class (Incomplete)

--- PRG-Assignment-2/IceCream.cs
+++ PRG-Assignment-2/IceCream.cs
@@ -7,6 +7,35 @@ using System.Threading.Tasks;
 namespace PRG_Assignment_2
 {
     abstract class IceCream
-    {
+    {   
+        //Properties
+        public string option { get; set; }
+        public int scoops { get; set; }
+
+        private List<Flavour> flavours; // Assuming a list of strings for flavor
+
+        public List<Flavour> Flavours
+        {
+            get { return flavours; }
+            set { flavours = value; }
+        }
+
+        private List<Topping> toppings;
+        public List <Topping> Toppings
+        {
+            get { return toppings; }
+            set { toppings = value; }
+        }
+
+        //Constructors
+        public IceCream() { }
+        public IceCream(string o, int s, List<Flavour> f, List<Topping> t)
+        {
+            Option = o;
+            Scoops = s;
+            Flavours = f;  
+            Toppings = t;   
+        }
+
     }
 }
--- PRG-Assignment-2/Program.cs
+++ PRG-Assignment-2/Program.cs
@@ -1,8 +1,9 @@
 ﻿//===============================//
 // Student Number : S10258483F
 // Student Name : Yong Xiang
-// Partner Name : Xavier
-// Person who committed : Yong Xiang
+// Student Number : S10257401E
+// Student Name : Xavier
+// Person who committed : Xavier Lian
 //===============================//
 
 
