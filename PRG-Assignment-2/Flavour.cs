decfd72256cf2736d9a136609e45d46dccf7c027
Pyongxiang <s10258483@connect.np.edu.sg>
2024-01-17 16:49:00 +0800

    Flavour and topping class ( incomplete )

---
+++ PRG-Assignment-2/Flavour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Assignment_2
+{
+    class Flavour
+    {
+        public string Type { get; set; }
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+        public Flavour() { }
+
+        public Flavour(string type, bool premium, int quantity)
+        {
+            Type = type;
+            Premium = premium;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Type : {Type}, Premium : {Premium}, Quantity : {Quantity}";
+        }
+    }
+}
---
+++ PRG-Assignment-2/Topping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Assignment_2
+{
+    class Topping
+    {
+        public string Type { get; set; }
+        public Topping() { }
+
+        public Topping(string type)
+        {
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Type : {Type}";
+        }
+    }
+}
